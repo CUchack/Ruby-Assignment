7c7e90cb88138f05d556992be263d9c7f83eb935
saasbook <saasbook@saasbook.(none)>
2014-02-05 16:27:06 -0700

    glob strings

---
+++ 6 Strings and Expressions new.rb
@@ -0,0 +1,17 @@
+def glob_match(filenames,pattern)
+  pattern.gsub!(/[\*\?\.]/,'*' => '.*', '.' => '\.', '?' => '.')
+  regex = Regexp.new(pattern)
+  filenames.select do |filename|
+    filename =~regex
+  end
+
+
+end
+
+puts '
+input = puts glob_match(["part1.rb","part2.rb","part2.rb~",".part3.rb.un~"],"*part*rb?*")
+
+output:'
+
+
+puts glob_match(["part1.rb","part2.rb","part2.rb~",".part3.rb.un~"],"*part*rb?*")
