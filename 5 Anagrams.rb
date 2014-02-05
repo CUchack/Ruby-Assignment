53a99630dcf6739fa50ec3f4bf98cdc7bec8594e
saasbook <saasbook@saasbook.(none)>
2014-02-04 21:34:44 -0700

    added anagrams

---
+++ 5 Anagrams.rb
@@ -0,0 +1,38 @@
+def combine_anagrams(words)
+
+  result = Array.new;
+  array = Array.new;
+  i = 0;
+  j = i+1;
+  while words.length > 0
+    array << words[i];
+    #print words;
+    while j < words.length
+      found_anagram = false;
+      if words[i].downcase.split("").sort.join == words[j].downcase.split("").sort.join
+        #print "anagram je";
+        found_anagram = true;
+        array << words[j];
+        words.delete_at(j);
+      end
+      if found_anagram == false
+        j+= 1;
+      end
+    end
+    #print array;
+    words.delete(words[i]);
+    result<<array;
+    array = [];
+    j = 1;
+  end
+  print result
+  return result
+end
+
+input = ['cars', 'for', 'potatoes', 'racs', 'four', 'scar', 'creams', 'scream']
+puts input
+combine_anagrams(input)
+
+
+#Code adapted from
+#https://github.com/epson121/Ruby/blob/master/anagrams.rb
